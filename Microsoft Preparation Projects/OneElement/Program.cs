fc7ea55bca001da10c840b2d001c9c5177450e1d
Gaurav Palande <gaurav.palande147@gmail.com>
2017-07-17 21:48:24 -0700

    OneElement Question solved

---
+++ Microsoft Preparation Projects/OneElement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 1, 12, 3, 12, 1, 1, 2, 3, 3 };
+            int ones = 0, twos = 0;
+            int commonMask;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                twos = twos | (ones & arr[i]);
+                ones = ones ^ arr[i];
+
+                commonMask = ~(ones & twos);
+
+                ones &= commonMask;
+                twos &= commonMask;
+            }
+            Console.WriteLine(ones);
+            Console.ReadLine();
+        }
+    }
+}
