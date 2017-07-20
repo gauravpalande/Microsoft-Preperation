02f4d1c6b626a3658d3fa6cf47973f994b9a3c30
Gaurav Palande <gaurav.palande147@gmail.com>
2017-07-20 16:43:07 -0700

    k largest(or smallest) elements in an array | added Min Heap method problem solved

---
+++ Microsoft Preparation Projects/KLargestOrSmallestElements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLargestOrSmallestElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(null).Select(x => Convert.ToInt32(x)).ToArray();
+
+            Array.Sort(array);
+
+            Console.WriteLine("How many elements do you want?");
+            int noElements = Convert.ToInt32(Console.ReadLine());
+            int[] outputArray = new int[noElements];
+
+            Console.WriteLine("Enter 1 for largest or 2 for smallest?");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    outputArray = array.Skip(array.Length-noElements).ToArray();
+                    foreach (int element in outputArray)
+                    {
+                        Console.WriteLine(element);
+                    }
+                    break;
+
+                case 2:
+                    outputArray = array.Take(noElements).ToArray();
+                    foreach (int element in outputArray)
+                    {
+                        Console.WriteLine(element);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
