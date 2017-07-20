24e6fd0d5cc2f10e7edbb4a935d72cbfde69d394
Gaurav Palande <gaurav.palande147@gmail.com>
2017-07-20 15:54:07 -0700

    Solved Kadane's Algorithm

---
+++ Microsoft Preparation Projects/Kadane's Algorithm/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kadane_s_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int noTestCases = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < noTestCases; i++)
+            {
+                int arrayLength = Convert.ToInt32(Console.ReadLine());
+                int[] array = Console.ReadLine().Split(null).Select(x => Convert.ToInt32(x)).ToArray();
+                int finalSum = int.MinValue;
+                int tempSum = 0;
+
+                for (int j = 0; j < arrayLength; j++)
+                {
+                    tempSum += array[j];
+
+                    if (tempSum > finalSum)
+                    {
+                        finalSum = tempSum;
+                    }
+                    else
+                    {
+                        tempSum = 0;
+                    }
+                }
+                Console.WriteLine(finalSum);
+            }
+            Console.ReadLine();
+        }
+    }
+}
