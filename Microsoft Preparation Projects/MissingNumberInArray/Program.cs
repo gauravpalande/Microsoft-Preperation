d0dcebeb9486eba572ac45bd92d6ac3aa81fe65c
Gaurav Palande <gaurav.palande147@gmail.com>
2017-07-20 20:40:52 -0700

    Solved Missing number in array problem

---
+++ Microsoft Preparation Projects/MissingNumberInArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingNumberInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int noTestCases = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < noTestCases; i++)
+            {
+                int arrayLength = Convert.ToInt32(Console.ReadLine());
+                int[] array = Console.ReadLine().Split(null).Select(x => Convert.ToInt32(x)).ToArray();
+
+                int formulaSum = (arrayLength * (arrayLength + 1)) / 2;
+                int arraySum = 0;
+
+                for (int j = 0; j < arrayLength - 1; j++)
+                {
+                    arraySum += array[j];
+                }
+
+                Console.WriteLine(formulaSum - arraySum);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
