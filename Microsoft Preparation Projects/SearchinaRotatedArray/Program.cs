ca33ef2824326925a502ebc3842cd21951d5e795
Gaurav Palande <gaurav.palande147@gmail.com>
2017-07-22 14:36:22 -0700

    Search in a Rotated Array problem solved

---
+++ Microsoft Preparation Projects/SearchinaRotatedArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchinaRotatedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int noOfTestCases = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < noOfTestCases; i++)
+            {
+                int arrayLength = Convert.ToInt32(Console.ReadLine());
+                int[] array = Console.ReadLine().Split(null).Select(x => Convert.ToInt32(x)).ToArray();
+                int numberToBeFound = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(Array.IndexOf(array, numberToBeFound));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
