4c9cacbdd40769fd06e1db7c4dfd511ae01e915d
Gaurav Palande <gaurav.palande147@gmail.com>
2017-07-21 20:29:11 -0700

    Find Maximum 6 Problem Solved

---
+++ Microsoft Preparation Projects/FindMaximum6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(null).Select(x => Convert.ToInt32(x)).ToArray();
+            int[] maximum = new int[6];
+
+            for (int i = 0; i < maximum.Length; i++)
+            {
+                maximum[i] = int.MinValue;
+            }
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                int minimum = maximum.Min();
+                if (array[j] > minimum)
+                {
+                    maximum[Array.IndexOf(maximum, minimum)] = array[j];
+                }
+            }
+
+            for (int k = 0; k < maximum.Length; k++)
+            {
+                Console.WriteLine(maximum[k]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
