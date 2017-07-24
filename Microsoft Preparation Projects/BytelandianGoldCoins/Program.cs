cac1ab23720221822e4c14128dc12ac89d740c84
Gaurav Palande <gaurav.palande147@gmail.com>
2017-07-23 19:04:42 -0700

    Linked List Middle Problem Solved

---
+++ Microsoft Preparation Projects/BytelandianGoldCoins/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BytelandianGoldCoins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Microsoft Preparation Projects/LinkedListMiddle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListMiddle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(new LinkedListNode<int>(1));
+            list.AddLast(new LinkedListNode<int>(2));
+            list.AddLast(new LinkedListNode<int>(3));
+            list.AddLast(new LinkedListNode<int>(4));
+            list.AddLast(new LinkedListNode<int>(5));
+
+            int listLength = list.Count;
+            LinkedListNode<int> foreRunner = list.First;
+            LinkedListNode<int> runner = list.First;
+
+
+            for (int i = 0; i < listLength; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    runner = runner.Next;
+                }
+                foreRunner = foreRunner.Next;
+            }
+
+            Console.WriteLine(runner.Value);
+            Console.ReadLine();
+        }
+    }
+}
