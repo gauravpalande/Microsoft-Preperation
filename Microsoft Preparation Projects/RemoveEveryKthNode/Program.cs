6ab1c87007dd251502946b66b56ca4ef3fbadcc9
Gaurav Palande <gaurav.palande147@gmail.com>
2017-07-23 21:22:04 -0700

    Remove Every Kth Node Problem Solved

---
+++ Microsoft Preparation Projects/RemoveEveryKthNode/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveEveryKthNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(new LinkedListNode<int>(1));
+            list.AddLast(new LinkedListNode<int>(2));
+            list.AddLast(new LinkedListNode<int>(3));
+            list.AddLast(new LinkedListNode<int>(4));
+            list.AddLast(new LinkedListNode<int>(5));
+
+            LinkedListNode<int> node = list.First;
+            int k = 2;
+
+            for (int i = 1; i <= list.Count; i++)
+            {
+                if (node != null)
+                {
+                    LinkedListNode<int> nextNode = node.Next;
+                    if (i % k == 0)
+                    {
+                        list.Remove(node);
+                    }
+                    node = nextNode;
+                }
+            }
+
+            do
+            {
+                Console.WriteLine(list.First.Value);
+                list.RemoveFirst();
+            } while (list.Count > 0);
+
+            Console.ReadLine();
+        }
+    }
+}
